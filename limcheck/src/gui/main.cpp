3026d33b3dbca690d55ec7a65ee0300a146e97a5
pbtorrild <pbtorrild@gmail.com>
2022-05-17 15:06:01 +0200

    not_working

--- limcheck/src/gui/main.cpp
+++ limcheck/src/gui/main.cpp
@@ -1,15 +1,17 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QFontDatabase>
+#include <ros_functions.h>
 
 
 int main(int argc, char *argv[])
 {
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+    qmlRegisterType<ROS_QT_BRIDGE>("ROS_QT_BRIDGE", 1, 0, "RosQtBridge");
 
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
+    #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    #endif
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
