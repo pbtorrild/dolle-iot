3ec09bd4f5d30cb9612bef4fa93b13d490939dfc
pbtorrild <pbtorrild@gmail.com>
2022-08-18 10:09:10 +0200

    Added function to save data from both cameras

---
+++ limcheck/launch/gather_data.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='limcheck',
+            namespace='sm2',
+            executable='saveData',
+            name='limcheck_1',
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                {"camera"   :   "1"}
+            ]
+        ),
+        Node(
+            package='limcheck',
+            namespace='sm2',
+            executable='saveData',
+            name='limcheck_0',
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                {"camera"   :   "0"}
+            ]
+        )        
+    ])
